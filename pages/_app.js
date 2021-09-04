85b22c2f8bb4e8be60edd7a211e0bf7ad8548bbd
Zavier Ferodova Al Fitroh <zavierferodova@gmail.com>
2021-09-04 15:01:20 +0700

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any
+}
+
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
